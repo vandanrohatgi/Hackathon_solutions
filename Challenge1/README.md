d0cd8114025347d79455a646a2316acb56e4f749
vandan rohatgi <43648786+vandanrohatgi@users.noreply.github.com>
2021-12-03 10:46:26 +0530

    Delete Challenge1 directory

--- Challenge1/README.md
+++
@@ -1,3 +0,0 @@
-So this was an easy one. We have been given a LAMP application. It had SQL injections and XSS vulnerabilities. 
-
-For the SQL injections I just replaced old MySQL queries with prepared statements. For XSS I used the htmlspecialchars() function.
--- Challenge1/fixed/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbuser = "newuser";
-    $dbpass = "password";
-    $dbname = "secure_coding";
-    $host = "localhost";
-    error_reporting(0);
-    $connection = mysqli_connect($host, $dbuser, $dbpass, $dbname) or die("Connection Failed: ".mysqli_connect_errno());
-?>
